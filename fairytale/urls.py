23392e56e7d8b0b30bfe092b5c6a06de81d9ef6a
Mamun Khondoker <mamun1980@gmail.com>
2020-02-13 06:03:45 +0600

    updated login and singup templates

--- fairytale/urls.py
+++ fairytale/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name="home"),
     path('accounts/login/',
          LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path('accounts/login/',
-         LoginView.as_view(template_name="accounts/login.html"), name="register"),
+     path('accounts/register/',
+         TemplateView.as_view(template_name="accounts/register.html"), name="register"),
+    path('accounts/logout/',
+         LogoutView.as_view(next_page='/'), name="logout"),
     path('admin/', admin.site.urls),
 ]
